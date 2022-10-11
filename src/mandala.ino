33b3be48634314f97cf326082a27bc45c165e9ff
Robert Edger <rbedger@gmail.com>
2022-10-10 20:11:17 -0500

    cycle mode; integrate button

--- src/mandala.ino
+++ src/mandala.ino
@@ -8,10 +8,10 @@
 
 #include "FastLED.h"
 
-#define DATA_PIN D0
+#define DATA_PIN D4
 
-#define MODE_PIN D5
-#define ANALOG_THRESHOLD 1000
+#define MODE_PIN A0
+#define ANALOG_THRESHOLD 900
 
 #define NUM_LEDS_RING 31
 #define NUM_MANDALA_LEGS 4
@@ -26,12 +26,14 @@
 
 CRGB leds[NUM_LEDS];
 
+#define NUM_MODES 5
 enum Mode
 {
-  Boot = 0,
+  Cycle = 0,
   Chase = 1,
   SlowFade = 2,
   Fill = 3,
+  StarSpin = 4,
 };
 
 enum OnOff
@@ -45,7 +47,8 @@ unsigned long lastModePressMillis = 0;
 unsigned long lastLoopMillis = 0;
 
 OnOff prevBtnState = OnOff::OFF;
-Mode curMode = Mode::SlowFade;
+Mode curMode = Mode::Cycle;
+Mode cycleMode = Mode::Chase;
 
 void setup()
 {
@@ -57,8 +60,20 @@ void setup()
 
 void loop()
 {
-  // readMode();
-  displayMode();
+  readMode();
+  if (curMode == Mode::Cycle)
+  {
+    displayMode(cycleMode);
+
+    if (millis() - lastLoopMillis > 10000)
+    {
+      cycleMode = (Mode)((((int)cycleMode) + 1) % NUM_MODES);
+    }
+  }
+  else
+  {
+    displayMode(curMode);
+  }
 
   FastLED.show();
 
@@ -67,7 +82,8 @@ void loop()
 
 void readMode()
 {
-  OnOff curBtnState = analogRead(MODE_PIN) < ANALOG_THRESHOLD
+  int buttonReading = analogRead(MODE_PIN);
+  OnOff curBtnState = buttonReading < ANALOG_THRESHOLD
                           ? OnOff::OFF
                           : OnOff::ON;
 
@@ -77,7 +93,7 @@ void readMode()
     // i.e. transitioning to OFF from a brightness change should not change mode.
     if (prevBtnState == OnOff::ON && modePressLoopCount <= MIN_BRIGHTNESS_LOOPS)
     {
-      curMode = (Mode)((((int)curMode) + 1) % 1);
+      curMode = (Mode)((((int)curMode) + 1) % NUM_MODES);
 
       Serial.print("Mode switched to ");
       Serial.println(curMode);
@@ -100,8 +116,8 @@ void readMode()
       {
         FastLED.setBrightness((FastLED.getBrightness() + 25) % PROJECT_MAX_BRIGHTNESS);
 
-        // Serial.print("Changed brightness to: ");
-        // Serial.println(FastLED.getBrightness());
+        Serial.print("Changed brightness to: ");
+        Serial.println(FastLED.getBrightness());
 
         lastModePressMillis = now;
       }
@@ -115,15 +131,14 @@ int currentRedIndex = 0;
 int currentBlueIndex = NUM_LEDS_RING / 3;
 int currentGreenIndex = NUM_LEDS_RING / 3 * 2;
 
-void displayMode()
+void displayMode(
+    Mode curMode)
 {
   FastLED.clearData();
 
   switch (curMode)
   {
-  case Mode::Boot:
-    fill_solid(leds, NUM_LEDS, CRGB::White);
-    break;
+  case Mode::Cycle:
   case Mode::Chase:
     displayChase(
         CRGB::Red,
@@ -143,6 +158,9 @@ void displayMode()
     break;
   case Mode::Fill:
     displayFill();
+    break;
+  case Mode::StarSpin:
+    displayStarSpin();
     break;
   }
 }
@@ -167,18 +185,18 @@ int mapPixel(
       legIndex = indexAfterRing / NUM_LEDS_MANDALA_LEG;
     }
 
-    Serial.printf("Leg Index: %d", legIndex);
+    // Serial.printf("Leg Index: %d", legIndex);
 
     switch (legIndex)
     {
     case 0:
       // the first leg is physically before the ring, but logically after
-      Serial.println("Case 0");
+      // Serial.println("Case 0");
       retVal = (index + 1) % NUM_LEDS_MANDALA_LEG;
       break;
     case 1:
     {
-      Serial.println("Case 1");
+      // Serial.println("Case 1");
       // the second leg runs INWARD, across from the first leg
       // thus, it needs to reverse direction
       int endOfLeg = NUM_LEDS_RING + NUM_LEDS_MANDALA_LEG * 2 - 1;
@@ -188,13 +206,13 @@ int mapPixel(
     }
     case 2:
     case 3:
-      Serial.println("Case 2/3");
+      // Serial.println("Case 2/3");
       retVal = NUM_LEDS_RING + (NUM_LEDS_MANDALA_LEG * legIndex) + (indexAfterRing % NUM_LEDS_MANDALA_LEG);
       break;
     }
   }
 
-  Serial.printf("Requested [%d], returned [%d]\n", index, retVal);
+  // Serial.printf("Requested [%d], returned [%d]\n", index, retVal);
 
   delay(100);
 
@@ -260,4 +278,17 @@ void displayFill()
   }
 
   delay(200);
+}
+
+int starSpinLegNum = 0;
+CRGB lastLegLed;
+
+void displayStarSpin()
+{
+  int firstLegLed = mapPixel(NUM_LEDS_RING + (starSpinLegNum++ % NUM_MANDALA_LEGS) * NUM_LEDS_MANDALA_LEG);
+
+  fill_rainbow(&leds[firstLegLed], NUM_LEDS_MANDALA_LEG, lastLegLed);
+  lastLegLed = leds[firstLegLed + NUM_LEDS_MANDALA_LEG - 1];
+
+  delay(250);
 }
